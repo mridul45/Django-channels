b4fd714bb6bf5801b721477aefae4d36fc95fb1f
Mridul Singhal <73380291+mridul45@users.noreply.github.com>
2022-09-14 12:29:57 +0530

    Add files via upload

--- chat/routing.py
+++ chat/routing.py
@@ -1,5 +1,7 @@
 from django.urls import re_path
 
-websocket_urlpattern = [
-	re_path(r'ws/chat/(?P<room_name>\w+)',consumers.ChatRoomConsumers),
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatRoomConsumer),
 ]
